a8ef79ead50294b30c8662a08d677206024f7e1b
nadirhadd <84446224+nadirhadd@users.noreply.github.com>
2023-01-10 22:10:37 +0700

    Add files via upload

---
+++ tubesAKA.go
@@ -0,0 +1,46 @@
+// selection sort
+package main
+
+func Selection(A tabInt, N int) {
+	var pass, idx, i, temp int
+	pass = 1
+	for pass <= N-1 {
+		idx = pass - 1
+		i = pass
+		for i < N {
+			if A[idx] < A[i] {
+				idx = i
+			}
+			i++
+		}
+		temp = A[pass-1]
+		A[pass-1] = A[idx]
+		A[idx] = temp
+		pass++
+	}
+}
+
+//merge sort
+
+func Merge(left, right []int) []int {
+
+	size, i, j := len(left)+len(right), 0, 0
+	slice := make([]int, size, size)
+
+	for k := 0; k < size; k++ {
+		if i > len(left)-1 && j <= len(right)-1 {
+			slice[k] = right[j]
+			j++
+		} else if j > len(right)-1 && i <= len(left)-1 {
+			slice[k] = left[i]
+			i++
+		} else if left[i] < right[j] {
+			slice[k] = left[i]
+			i++
+		} else {
+			slice[k] = right[j]
+			j++
+		}
+	}
+	return slice
+}
